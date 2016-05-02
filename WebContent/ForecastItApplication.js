ecb7044b79e088d64c3c2ae5c440e1f4b342bcfb
Erika <ekiem16@holynames-sea.org>
2016-05-02 09:33:07 -0700

    character count

--- WebContent/ForecastItApplication.js
+++ WebContent/ForecastItApplication.js
@@ -2,11 +2,12 @@
  *  Javascript file for CollegeApplication App
  */
 document.addEventListener('DOMContentLoaded', function(){
-	document.getElementById('collegeNameInputBox').addEventListener('input', updateStatus);
+	document.getElementById('commentsInputBox').addEventListener('input', updateStatus);
 });
 
+
 function updateStatus(){
-	var text = document.getElementById('collegeNameInputBox').value;
+	var text = document.getElementById('commentsInputBox').value;
 	if(text != null && text.length != 0){
 		document.getElementById('formStatusBox').innerHTML='You have entered ' + text.length + ' characters';
 	} else {
